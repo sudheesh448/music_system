b729030b2bfd9389685ca68cbb42aa39a1ef94f9
sudheesh <sudheesh.u@netwebindia.com>
2024-01-05 14:46:34 +0530

    created the api call and functions for listing out the songs, the listed dictionary return include song id album id title artist album release year

---
+++ backend/functions/functions_for_listing.py
@@ -0,0 +1,26 @@
+from sqlalchemy.orm import Session
+from backend.models import Music
+
+def get_all_songs(db: Session):
+    """
+    Get a list of all songs in the database.
+
+    Parameters:
+    - db (Session): The database session.
+
+    Returns:
+    - List[dict]: List of dictionaries representing each song's details.
+    """
+
+    songs = db.query(Music).all()
+    return [
+        {
+            "song_id": song.id,
+            "album_id": song.album.id if song.album else None,
+            "title": song.title,
+            "artist": song.artist,
+            "album": song.album.title if song.album else None,
+            "release_year": song.release_year
+        }
+        for song in songs
+    ]
--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,11 @@ from fastapi import FastAPI, File, UploadFile, Form, HTTPException, Depends
 from sqlalchemy.orm import Session
 
 from backend.database import SessionLocal, engine
+# from backend.functions.functions_for_upload import is_mp3_file, save_music_details
+from backend.functions.functions_for_listing import get_all_songs
+from typing import List
 from backend.models import Music, Album
+
 
 app = FastAPI()
 
@@ -18,3 +22,16 @@ def get_db():
         db.close()
 
 
+@app.get("/api/music", response_model=List[dict])
+async def list_songs(db: Session = Depends(get_db)):
+    """
+    Get a list of all songs in the database.
+
+    Parameters:
+    - db (Session): The database session.
+
+    Returns:
+    - List[dict]: List of dictionaries representing each song's details.
+    """
+    songs = get_all_songs(db)
+    return songs
