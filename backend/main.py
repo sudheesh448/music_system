e187855f07a8f30307a5308e6d120c46a1deeb81
sudheesh <sudheesh.u@netwebindia.com>
2024-01-06 09:46:37 +0530

    created api call for getting the album details and associated songs of the album by using album id

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ from fastapi import FastAPI, File, UploadFile, Form, HTTPException, Depends
 from sqlalchemy.orm import Session
 
 from backend.database import SessionLocal, engine
-from backend.functions.functions_for_upload import is_mp3_file, save_music_details
+# from backend.functions.functions_for_upload import is_mp3_file, save_music_details
 from backend.functions.functions_for_listing import get_all_songs
 from typing import List
 from backend.models import Music, Album
@@ -35,3 +35,42 @@ async def list_songs(db: Session = Depends(get_db)):
     """
     songs = get_all_songs(db)
     return songs
+
+@app.get("/api/albums/{album_id}", response_model=dict)
+async def get_album_details(album_id: int, db: Session = Depends(get_db)):
+
+
+    """
+    api call for fetching the album details and associated songs in it
+    by using the album id
+
+    PARAMETERS:
+
+    RETURNS:
+    --------
+    Returns id, title, favorite of albums
+    and id, title, artist, release_year,favorite of associated songs
+
+    """
+
+    album = db.query(Album).filter(Album.id == album_id).first()
+
+    if album is None:
+        raise HTTPException(status_code=404, detail="Album not found")
+
+    songs = db.query(Music).filter(Music.album_id == album_id).all()
+
+    album_details = {
+        "id": album.id,
+        "title": album.title,
+        "favorite": album.favorite,
+        "songs": [{
+            "id": song.id,
+            "title": song.title,
+            "artist": song.artist,
+            "release_year": song.release_year,
+            "favorite":song.favorite
+        } for song in songs]
+    }
+
+    return album_details
