1debfa5a10b1be58aceb6f5ed3f1cd090842e7d8
sudheesh <sudheesh.u@netwebindia.com>
2024-01-06 17:17:30 +0530

    edited the doc string and added the try except block for the function get_all_albums

--- backend/functions/functions_for_listing_out_albums.py
+++ backend/functions/functions_for_listing_out_albums.py
@@ -1,24 +1,34 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from backend.models import Album
+from typing import List, Dict
 
-def get_all_albums(db: Session):
+def get_all_albums(db: Session) -> List[Dict[str, any]]:
     """
     Get a list of all albums in the database.
 
     -------------------------------
     PARAMETERS:
-    - db (Session): The database session.
+      
 
     RETURNS:
-    - List[dict]: List of dictionaries representing each album's details.
+        - List[dict]: List of dictionaries representing each album's details.
+          Each dictionary has the following keys:
+          - "album_id" (int): The unique identifier of the album.
+          - "title" (str): The title of the album.
+          - "favorite" (bool): Indicates whether the album is marked as a favorite.
     """
 
-    albums = db.query(Album).all()
-    return [
-        {
-            "album_id": album.id,
-            "title": album.title,
-            "favorite": album.favorite,
-        }
-        for album in albums
-    ]
+    try:
+        albums = db.query(Album).all()
+        return [
+            {
+                "album_id": album.id,
+                "title": album.title,
+                "favorite": album.favorite,
+            }
+            for album in albums
+        ]
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal Server Error")
