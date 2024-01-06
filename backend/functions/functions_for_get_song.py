25daae4b8b44c5cefad3c495563b8054cb2a81c0
sudheesh <sudheesh.u@netwebindia.com>
2024-01-06 17:13:20 +0530

    for get songs by id function edited the doc string and add try except block

--- backend/functions/functions_for_get_song.py
+++ backend/functions/functions_for_get_song.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from backend.models import Music
 from sqlalchemy.orm import Session
 
@@ -5,14 +6,34 @@ from sqlalchemy.orm import Session
 def get_song_by_id(db: Session, song_id: int):
     
     """
-    Function for fetching the song details by using song id from db.
+    Fetches the details of a song by its ID from the database.
 
-    PARAMETER:
-    song_id
+    PARAMETERS:
+        - song_id (int): The ID of the song.
 
     RETURNS:
-    Returning the song details
-
+        - dict: A dictionary representing the song details. The dictionary has the following keys:
+          - "id" (int): The unique identifier of the song.
+          - "title" (str): The title of the song.
+          - "artist" (str): The artist of the song.
+          - "release_year" (int): The release year of the song.
+          - "favorite" (bool): Indicates whether the song is marked as a favorite.
     """
 
-    return db.query(Music).filter(Music.id == song_id).first()
+    try:
+        song = db.query(Music).filter(Music.id == song_id).first()
+
+        if song is None:
+            raise HTTPException(status_code=404, detail="Song not found")
+
+        return {
+            "id": song.id,
+            "title": song.title,
+            "artist": song.artist,
+            "release_year": song.release_year,
+            "favorite": song.favorite,
+        }
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
